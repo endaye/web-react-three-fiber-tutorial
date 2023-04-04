1c2c56ec78ab131fb7a83468457b9f2e56463b1e
Yuancheng Zhang <old.far.hot@gmail.com>
2023-04-04 22:41:36 +0800

    update gltfjsx to add a chair

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,13 @@
 import { PresentationControls, Stage, MeshReflectorMaterial } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
 import { Suspense } from "react";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Chair from "./Chair";
 
 const Experience = () => {
-  const gltf = useLoader(GLTFLoader, "./models/chair.gltf");
-
   return (
     <PresentationControls speed={1.5} global={true} zoom={0.7} polar={[-0.1, Math.PI / 4]}>
       <Stage environment="city" intensity={0.6} castShadow={false}>
         <Suspense fallback={null}>
-          <primitive object={gltf.scene} scale={0.1} position={[0, -1.5, 0]} />
+          <Chair />
         </Suspense>
       </Stage>
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
